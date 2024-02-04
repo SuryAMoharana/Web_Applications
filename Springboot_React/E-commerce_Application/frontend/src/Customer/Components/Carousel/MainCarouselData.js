1e080b18ab6c62a0567a35684fd2534edacaa03a
Surya Moharana <suryachintu1994@gmail.com>
2024-02-04 20:47:05 +0530

    Halting
    Halting to research about more in it

---
+++ Springboot_React/E-commerce_Application/frontend/src/Customer/Components/Carousel/MainCarouselData.js
@@ -0,0 +1,17 @@
+export const mainCarouselData=[    
+    {
+        image:"https://www.dfupublications.com/images/2023/07/27/India%E2%80%99s%20Ethnic%20Wear%20Goes%20Global_large.jpg"
+    },
+    {
+        image:"https://www.dfupublications.com/images/2023/07/27/India%E2%80%99s%20Ethnic%20Wear%20Goes%20Global_large.jpg"    
+    },
+    {
+        image:"https://www.dfupublications.com/images/2023/07/27/India%E2%80%99s%20Ethnic%20Wear%20Goes%20Global_large.jpg"    
+    },
+    {
+        image:"https://www.dfupublications.com/images/2023/07/27/India%E2%80%99s%20Ethnic%20Wear%20Goes%20Global_large.jpg"    
+    },
+    {
+        image:"https://www.dfupublications.com/images/2023/07/27/India%E2%80%99s%20Ethnic%20Wear%20Goes%20Global_large.jpg"    
+    }
+]
---
+++ Springboot_React/E-commerce_Application/frontend/src/Customer/Pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import MainCarousel from '../Components/Carousel/MainCarousel'
+
+const HomePage = () => {
+  return (
+    <div>
+        <MainCarousel/>
+        <div>
+            Other Sections
+        </div>
+    </div>
+  )
+}
+
+export default HomePage
